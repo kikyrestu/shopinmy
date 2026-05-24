5683f116a354217082dff162f3e45727538fef58
kikyrestu <kikyrestu7@gmail.com>
2026-05-24 11:14:18 +0700

    fix: restore strict type hints for navigation properties in PaymentResource

--- app/Filament/Resources/Payments/PaymentResource.php
+++ app/Filament/Resources/Payments/PaymentResource.php
@@ -19,8 +19,8 @@ class PaymentResource extends Resource
 {
     protected static ?string $model = Payment::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
-    protected static ?string $navigationGroup = 'Sales';
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-currency-dollar';
+    protected static \UnitEnum|string|null $navigationGroup = 'Sales';
     protected static bool $shouldRegisterNavigation = false;
 
     public static function form(Schema $schema): Schema
